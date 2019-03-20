494badc63becf7f2d48d74a69cc64815ecbb0580
320_100008169054_justdoit <liudong_work@163.com>
2019-03-20 20:58:33 +0800

    getUserInfo

--- app.js
+++ app.js
@@ -15,11 +15,13 @@ App({
     // 获取用户信息
     wx.getSetting({
       success: res => {
+ 
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
+              console.log("返回的数据", res.userInfo)
               this.globalData.userInfo = res.userInfo
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
--- pages/component/car/car.js
+++ pages/component/car/car.js
@@ -1,4 +1,4 @@
-var app = getApp();
+var app = getApp(); 
 
 Component({
   
@@ -17,6 +17,10 @@ Component({
 
   /* 组件的初始数据 */
   data: {
+    personName:'',     //姓名 app.globalData.userInfo
+    phophoneNum:'',  //手机号
+    district:'',   //地区
+    districtDetail:'', //详细地址
     
   },
 
@@ -35,7 +39,18 @@ Component({
 
   /* 组件的方法列表 */
   methods: {
+    getInfoBtn:function(){
+      var that=this;
+      console.log(app.globalData.userInfo);
+      that.setData({
+        // personName: app.globalData.userInfo.nickName,
 
+      })
+
+
+    }
   }
+
+  
   
 })
